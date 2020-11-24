6ea10c904b769dc5de9b740ccb18b5974a2336e7
Bryan Coulier <bryan.coulier@hotmail.com>
2020-11-24 19:55:22 +0100

    CSS and cleanup

--- controllers/SubscriberController.js
+++ controllers/SubscriberController.js
@@ -4,6 +4,6 @@ const Subscriber = mongoose.model('Subscriber');
 exports.postPeople = async (req, res) => {
   const subscriber= new Subscriber(req.body);
   await subscriber.save();
-  req.flash('success', 'You have filled out the form!');
+  req.flash('success', 'You are succesfully added to the mailing list!');
   res.redirect('/mail');
 }
