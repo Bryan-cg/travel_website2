47575b2a81ace38032193d6f2f4b26db572fb3eb
Bryan Coulier <bryan.coulier@hotmail.com>
2020-11-29 17:42:01 +0100

    retrieve country names from db

--- controllers/countryController.js
+++ controllers/countryController.js
@@ -4,5 +4,10 @@ var City = require('../models/city');
 
 //DISPLAY list of all countries
 exports.country_list = async (req, res) => {
-    res.render('countries_list',{title: 'Countries'});
+    Country.find({}, 'name capital')
+    .exec(function (err, list_countries) {
+      if (err) { return next(err); }
+      //Successful, so render
+      res.render('countries_list', { title: 'Countries', countries_list: list_countries });
+    });
 };
